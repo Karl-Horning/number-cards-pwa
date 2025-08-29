3b49d371a6c1cb69b7fd4bf2847d89f01f2f4431
Karl Horning <k.horning@me.com>
2025-08-29 13:13:59 +0100

    build: add correct base to Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+    base: "/number-cards-pwa/",
     plugins: [
         tailwindcss(),
         react(),
